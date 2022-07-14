702a17d0c72e8900d5fd4a55c20642c0259074f5
zubakker <zubakker@gmail.com>
2022-07-14 21:03:32 +0000

    add CoffeeShop model

--- coffeeapp/main/models.py
+++ coffeeapp/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+# from django.contrib.gis.db import models as models
 
 # Create your models here.
+class CoffeeShop(models.Model):
+    name    = models.CharField(max_length=128)
+    address = models.JSONField(max_lenght=2048)
+    coordinates = models.JSONField(max_lenght=1024)
+    # opening_hours stored in OpenStreetMap opening_hours forman
+    opening_hours   = models.CharField(max_length=128, blank=True)
+    # photos = 
+    # coffee  = models.ManyToManyField(...) 
+
