2c5dfc4d6c00173b00a277c34b9c81ce07bf2ebe
zubakker <zubakker@gmail.com>
2022-07-15 18:01:53 +0000

    add Descriptor model

--- coffeeapp/main/admin.py
+++ coffeeapp/main/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import CoffeeShop
+from .models import CoffeeShop, CoffeeDrink, DrinkReview, Descriptor
 
 admin.site.register(CoffeeShop)
+admin.site.register(CoffeeDrink)
+admin.site.register(DrinkReview)
+admin.site.register(Descriptor)
--- coffeeapp/main/models.py
+++ coffeeapp/main/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-# from django.contrib.gis.db import models as models
+
 
 # Create your models here.
 class CoffeeShop( models.Model ):
     name    = models.CharField(max_length=128)
-    address = models.JSONField(max_lenght=2048)
-    coordinates = models.JSONField(max_lenght=1024)
+    address = models.JSONField(max_length=2048)
+    coordinates = models.JSONField(max_length=1024)
     # opening_hours stored in OpenStreetMap opening_hours format
     opening_hours   = models.CharField(max_length=128, blank=True)
 
@@ -13,8 +13,8 @@ class CoffeeShop( models.Model ):
 class CoffeeDrink( models.Model ):
     name    = models.CharField(max_length=128)
     parent_shop = models.ForeignKey(
-        'CoffeeShop',
-        on_delete=models.CASCADE,
+            'CoffeeShop',
+            on_delete=models.CASCADE,
     )
     drink_type  = models.CharField(max_length=128)
     description = models.TextField()
@@ -25,6 +25,18 @@ class DrinkReview( models.Model ):
     # user = foreignkey
 
     created = models.DateTimeField(auto_now_add=True)
-    quality = models.IntegerField() # from 0 to 10
-    # descriptors = foreignkey
+    quality = models.IntegerField() # 0 to 10
+    descriptors = models.ManyToManyField( 'Descriptor' )
+
+
+class Descriptor( models.Model ):
+    parent  = models.ForeignKey( 
+            'self',
+            on_delete=models.CASCADE
+    )
+    name    = models.CharField(max_length=64)
+    description = models.CharField(max_length=1024)
+    color   = models.CharField(max_length=6) # RGB hex
+    children    = models.ManyToManyField( 'self' )
+
 
