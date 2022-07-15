c2cec0e8c5350e7ed53dc2bea33a2f4f80f0fe6e
zubakker <zubakker@gmail.com>
2022-07-15 13:56:44 +0000

    add CoffeeDrink model

--- coffeeapp/main/models.py
+++ coffeeapp/main/models.py
@@ -6,8 +6,18 @@ class CoffeeShop(models.Model):
     name    = models.CharField(max_length=128)
     address = models.JSONField(max_lenght=2048)
     coordinates = models.JSONField(max_lenght=1024)
-    # opening_hours stored in OpenStreetMap opening_hours forman
+    # opening_hours stored in OpenStreetMap opening_hours format
     opening_hours   = models.CharField(max_length=128, blank=True)
-    # photos = 
-    # coffee  = models.ManyToManyField(...) 
+
+
+class CoffeeDrink(models.Model):
+    name    = models.CharField(max_length=128)
+    parent_shop = models.ForeignKey(
+        'CoffeeShop',
+        on_delete=models.CASCADE,
+    )
+    drink_type  = models.CharField(max_length=128)
+    description = models.TextField()
+    price   = models.FloatField()
+
 
